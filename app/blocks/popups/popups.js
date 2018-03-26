d118d4dfee82fd0137e6b7e84f33e9dfe02a348e
Allkrain <aukra25@yandex.ru>
2018-03-26 17:03:30 +0300

    popups

--- app/blocks/popups/popups.js
+++ app/blocks/popups/popups.js
@@ -10,4 +10,34 @@ export default function popups() {
     afterLoad: freeze,
     afterClose: unfreeze,
   });
+
+  $(document).on('click touchstart', '.js-popup', (evt) => {
+    evt.preventDefault();
+    const self = $(evt.target).hasClass('.js-popup') ? $(evt.target) : $(evt.target).closest('.js-popup');
+    const targetPopup = $(self).attr('href').split('#').pop();
+    const currentPopup = $('.popups').attr('data-active-popup');
+    const targetLink = $(self).data('target-news');
+    if ($('.popups').hasClass('is-active') && currentPopup !== targetPopup) {
+      $(`#${currentPopup}`).fadeOut(300);
+      $(`#${targetPopup}`).fadeIn(300);
+    } else {
+      freeze();
+      $('.popups').attr('data-active-popup', targetPopup);
+      $(`#${targetPopup}`).fadeIn(300);
+      $('.popups').addClass('is-active');
+      $('.overlay').addClass('is-active');
+      if (targetLink.length > 0) {
+        $('.popups__wrapper').stop().animate({
+          scrollTop: ($(`#${targetLink}`).offset().top - 60),
+        }, 1000, 'swing');
+      }
+    }
+  });
+  $(document).on('click touchstart', '.js-popup-close,.overlay', (evt) => {
+    evt.preventDefault();
+    unfreeze();
+    $('.popup').fadeOut();
+    $('.popups').removeClass('is-active').attr('data-active-popup', '');
+    $('.overlay').removeClass('is-active');
+  });
 }
