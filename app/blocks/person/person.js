a1efe8cb81486799f99a68efb176dfa966528882
Allkrain <aukra25@yandex.ru>
2018-05-11 16:42:28 +0300

    fixes

--- app/blocks/person/person.js
+++ app/blocks/person/person.js
@@ -1,13 +1,13 @@
-const $ = window.$;
-
-export default function showMore() {
-  $(document).on('click touchstart', '.js-show-more', (evt) => {
-    evt.preventDefault();
-    const et = evt.target;
-    const self = $(et).hasClass('js-show-more') ? $(et) : $(et).closest('.js-show-more');
-    const targetContainer = $(self).closest('.person__describe').find('.person__short-info');
-    const fullDescribe = $(self).closest('.person__describe').find('.person__full-info');
-    $(targetContainer).text($(fullDescribe).text());
-    $(self).fadeOut();
-  });
-}
+// const $ = window.$;
+//
+// export default function showMore() {
+//   $(document).on('click touchstart', '.js-show-more', (evt) => {
+//     evt.preventDefault();
+//     const et = evt.target;
+//     const self = $(et).hasClass('js-show-more') ? $(et) : $(et).closest('.js-show-more');
+//     const targetContainer = $(self).closest('.person__describe').find('.person__short-info');
+//     const fullDescribe = $(self).closest('.person__describe').find('.person__full-info');
+//     $(targetContainer).text($(fullDescribe).text());
+//     $(self).fadeOut();
+//   });
+// }
